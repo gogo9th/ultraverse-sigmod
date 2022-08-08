7582622772b2a355fdd5e7e5cd04c516e3fe3025
Gyuhwan Park★ <unstabler@unstabler.pl>
2022-08-08 16:42:32 +0900

    feature(mariadb): add wrapper class for MYSQL *

---
+++ src/mariadb/DBHandle.cpp
@@ -0,0 +1,36 @@
+//
+// Created by cheesekun on 8/8/22.
+//
+
+#include <stdexcept>
+
+#include <fmt/core.h>
+
+#include "DBHandle.hpp"
+
+
+namespace ultraverse::mariadb {
+    
+    DBHandle::DBHandle():
+        _handle(mysql_init(nullptr), mysql_close)
+    {
+    
+    }
+    
+    void DBHandle::connect(const std::string &host, int port, const std::string &user, const std::string &password) {
+        mysql_real_connect(_handle.get(), host.c_str(), user.c_str(), password.c_str(), nullptr, port, nullptr, 0);
+        if (mysql_errno(_handle.get()) != 0) {
+            throw std::runtime_error(
+                fmt::format("mysql_real_connect returned {}.", mysql_errno(_handle.get()))
+            );
+        }
+    }
+    
+    void DBHandle::disconnect() {
+        mysql_close(_handle.get());
+    }
+    
+    std::shared_ptr<MYSQL> DBHandle::handle() {
+        return _handle;
+    }
+}
---
+++ src/mariadb/DBHandle.hpp
@@ -0,0 +1,28 @@
+//
+// Created by cheesekun on 8/8/22.
+//
+
+#ifndef ULTRAVERSE_DBHANDLE_HPP
+#define ULTRAVERSE_DBHANDLE_HPP
+
+#include <memory>
+
+#include <mysql/mysql.h>
+
+namespace ultraverse::mariadb {
+    class DBHandle {
+    public:
+        explicit DBHandle();
+        DBHandle(DBHandle &) = delete;
+        
+        void connect(const std::string &host, int port, const std::string &user, const std::string &password);
+        void disconnect();
+        
+        std::shared_ptr<MYSQL> handle();
+        
+    private:
+        std::shared_ptr<MYSQL> _handle;
+    };
+}
+
+#endif //ULTRAVERSE_DBHANDLE_HPP
