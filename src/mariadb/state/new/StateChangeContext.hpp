d1c8d802f682c0daafe4342c73e60e91d6ebb18e
Gyuhwan Park★ <unstabler@unstabler.pl>
2022-09-19 19:02:55 +0900

    feature(NamingHistory): 테이블 / 필드 이름의 naming history를 담을 수 있는 클래스 작성

--- src/mariadb/state/new/StateChangeContext.hpp
+++ src/mariadb/state/new/StateChangeContext.hpp
@@ -11,15 +11,42 @@
 #include <list>
 #include <map>
 
+#include <mutex>
+
+#include "cluster/NamingHistory.hpp"
+
 namespace ultraverse::state::v2 {
-    struct RenameHistory {
-        uint64_t time;
-        std::string name;
+    struct ForeignKey {
+        std::shared_ptr<NamingHistory> fromTable;
+        std::string fromColumn;
+        
+        std::shared_ptr<NamingHistory> toTable;
+        std::string toColumn;
     };
     
     class StateChangeContext {
     public:
-        std::map<std::string, std::list<RenameHistory>> renameHistoryMap;
+        std::shared_ptr<NamingHistory> findTable(std::string tableName, uint64_t when) {
+            auto it = std::find_if(tables.begin(), tables.end(), [tableName, when](auto &history) {
+                return history->match(tableName, when);
+            });
+            
+            if (it != tables.end()) {
+                return *it;
+            } else {
+                // FIXME: 이거 parseDDL에서 해야 함
+                auto table = std::make_shared<NamingHistory>(tableName);
+                tables.push_back(table);
+                
+                return table;
+            }
+        }
+        
+        std::vector<std::shared_ptr<NamingHistory>> tables;
+        std::vector<ForeignKey> foreignKeys;
+        
+        // fixme;
+        std::mutex contextLock;
     };
 }
 
