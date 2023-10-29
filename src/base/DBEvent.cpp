67b9d2da50c4f1f33e5f88db147b066be5a0c64e
Gyuhwan Park★ <unstabler@unstabler.pl>
2023-10-30 03:46:24 +0900

    Revert "fix: 잘못 롤백 처리된 소스 코드 복원"
    This reverts commit d42597bd316d6d3cffa13b0fc041b3af2e4c3955.

--- src/base/DBEvent.cpp
+++ src/base/DBEvent.cpp
@@ -18,8 +18,7 @@
 
 
 namespace ultraverse::base {
-
- 
+    
     QueryEventBase::QueryEventBase():
         _logger(createLogger("QueryEventBase")),
         _queryType(UNKNOWN)
@@ -102,26 +101,10 @@ namespace ultraverse::base {
                 _itemSet.begin(), _itemSet.end()
             );
         } else if (_queryType == UPDATE) {
-            {
-                auto it = _itemSet.begin();
-                
-                while (true) {
-                    it = std::find_if(it, _itemSet.end(), [this](const StateItem &item) {
-                        return std::any_of(_writeColumns.begin(), _writeColumns.end(), [&item](const std::string &colName) {
-                            return item.name == colName;
-                        });
-                    });
-                    
-                    if (it == _itemSet.end()) {
-                        break;
-                    }
-                    
-                    _writeItems.emplace_back(*it);
-                    
-                    it++;
-                }
-            }
-            
+            _writeItems.insert(
+                _writeItems.end(),
+                _itemSet.begin(), _itemSet.end()
+            );
             
             _readItems.insert(
                 _readItems.end(),
@@ -219,9 +202,6 @@ namespace ultraverse::base {
         return true;
     }
     
-    /**
-     * UPDATE 쿼리의 파싱 결과로부터 readSet, writeSet을 추출한다.
-     */
     bool QueryEventBase::processUpdate(const ultparser::DMLQuery &dmlQuery) {
         const std::string primaryTable = dmlQuery.table().real().identifier();
         _relatedTables.insert(primaryTable);
