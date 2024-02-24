482db21fc9339c2c6e5b49189e59add53d324c41
Gyuhwan Park <unstabler@unstabler.pl>
2024-02-24 17:52:50 +0900

    Revert "checkpoint"
    This reverts commit 0326482307e9e55ae11817b3f0a91d4b5f1d7fab.

--- src/base/DBEvent.cpp
+++ src/base/DBEvent.cpp
@@ -101,25 +101,10 @@ namespace ultraverse::base {
                 _itemSet.begin(), _itemSet.end()
             );
         } else if (_queryType == UPDATE) {
-            {
-                auto it = _itemSet.begin();
-                
-                while (true) {
-                    it = std::find_if(it, _itemSet.end(), [this](const StateItem &item) {
-                        return std::any_of(_writeColumns.begin(), _writeColumns.end(), [&item](const std::string &colName) {
-                            return item.name == colName;
-                        });
-                    });
-                    
-                    if (it == _itemSet.end()) {
-                        break;
-                    }
-                    
-                    _writeItems.emplace_back(*it);
-                    
-                    it++;
-                }
-            }
+            _writeItems.insert(
+                _writeItems.end(),
+                _itemSet.begin(), _itemSet.end()
+            );
             
             _readItems.insert(
                 _readItems.end(),
